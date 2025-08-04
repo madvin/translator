878a809886f51348cf6a5e457ac763b3f0d10dc1
madvin <madvin@abv.bg>
2025-08-04 20:05:57 +0300

    start of designing the fonts

--- translater/src/components/Header.jsx
+++ translater/src/components/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
     <header className="flex items-center justify-between gap-4 p-4">
-      <h1>
-        Free<span className="text-blue-500">Translater</span>
+      <h1 className='font-medium text-3xl sm:text-4xl md:text-5xl'>
+        Free<span className="text-blue-400 bold">Translater</span>
       </h1>
       <button className="flex items-center justify-center gap-2 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors">
         <p>New</p>
--- translater/src/components/HomePage.jsx
+++ translater/src/components/HomePage.jsx
@@ -1,3 +1,7 @@
 export default function HomePage() {
-  return <main className="flex-1 p-4 flex flex-col justify-center"></main>;
+  return (
+  <main className="flex-1 p-4 flex flex-col gap-3 flex-col sm:gap-4 md:gap-5">
+    <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Free<span className='text-blue-400 bold font-semibold'>Translate</span></h1>
+  </main>
+  )
 }
