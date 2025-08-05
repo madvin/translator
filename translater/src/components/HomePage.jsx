c8f0c0251d4ec5da97c955fa37f12b3bf1c55b88
madvin <madvin@abv.bg>
2025-08-05 22:11:05 +0300

    add upload and record itemms

--- translater/src/components/HomePage.jsx
+++ translater/src/components/HomePage.jsx
@@ -1,8 +1,19 @@
 export default function HomePage() {
   return (
-  <main className='flex-1 p-4 flex flex-col gap-3 text-center flex-col text-center sm:gap-4 md:gap-5'>
-    <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Free<span className='text-blue-400 bold font-semibold'>Translate</span></h1>
-    <h3 className='font-medium'>Record <span className='text-blue-400'>&rarr;</span>Transcribe<span className='text-blue-400'>&rarr;</span>Translate</h3>
-  </main>
-  )
+    <main className="flex-1 p-4 flex flex-col gap-3 text-center flex-col text-center sm:gap-4 md:gap-5">
+      <h1 className="font-semibold text-5xl sm:text-6xl md:text-7xl">
+        Free<span className="text-blue-400 bold font-semibold">Translate</span>
+      </h1>
+      <h3 className="font-medium md:text-lg">
+        Record <span className="text-blue-400">&rarr;</span>Transcribe
+        <span className="text-blue-400">&rarr;</span>Translate
+      </h3>
+      <button className="flex items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4">
+        <p>Record</p>
+        <i className="fa-solid fa-microphone"></i>
+      </button>
+      <p>Or <label className="text-blue cursor-pointer hover:text-blue-600 duration-200">upload <input className="hidden" type="file" accept=".mp3 .mp4 .wave"  /></label> a mp3 file</p>
+      <p className="italic text-slate-400">Something that will be free forever</p>
+    </main>
+  );
 }
