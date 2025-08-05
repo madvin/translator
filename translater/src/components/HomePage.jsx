666f35ca5d951e608d8946aed105c56dab1a5fe4
madvin <madvin@abv.bg>
2025-08-05 08:54:18 +0300

    centered text

--- translater/src/components/HomePage.jsx
+++ translater/src/components/HomePage.jsx
@@ -1,6 +1,6 @@
 export default function HomePage() {
   return (
-  <main className='flex-1 p-4 flex flex-col gap-3 flex-col sm:gap-4 md:gap-5'>
+  <main className='flex-1 p-4 flex flex-col gap-3 text-center flex-col text-center sm:gap-4 md:gap-5'>
     <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Free<span className='text-blue-400 bold font-semibold'>Translate</span></h1>
     <h3 className='font-medium'>Record <span className='text-blue-400'>&rarr;</span>Transcribe<span className='text-blue-400'>&rarr;</span>Translate</h3>
   </main>
