aec730bb30f541a3d908a86eeda3af13a3e4313c
madvin <madvin@abv.bg>
2025-08-11 20:37:04 +0300

    add FileDisplay component and its props and button elements

--- translater/src/App.jsx
+++ translater/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import HomePage from "./components/HomePage";
 import Header from "./components/Header";
+import FileDisplay from "./components/FileDisplay";
 
 function App() {
   const [file, setFile] = useState(null);
@@ -18,9 +19,12 @@ function App() {
       <section className="min-h-screen flex flex-col">
         <Header />
       { isAudioAvailable ? (
-        <FileDisplay handleAudioReset={handleAudioReset} file={file} audioStream={setAudioStream} />
+        <FileDisplay handleAudioReset=
+        {handleAudioReset} file={file} audioStream=
+        {setAudioStream} />
       ) : (
-      <HomePage setFile = {setFile} setAudioStream = {setAudioStream} />
+      <HomePage setFile={setFile} setAudioStream=
+      {setAudioStream} />
       ) }
       </section>
       <footer></footer>
--- translater/src/components/FileDisplay.jsx
+++ translater/src/components/FileDisplay.jsx
@@ -5,6 +5,21 @@ export default function FileDisplay(props) {
       <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
         Your <span className="text-blue-400 bold font-semibold">File</span>
       </h1>
+      <div className="flex items-center gap-2">
+        <h3 className="font-semibold">Name</h3>
+        <p>{file.name}</p>
+      </div>
+      <div className="flex items-center justify-between gap-4">
+        <button
+          onClick={() => audioStream(null)}
+          className="specialBtn px-4 py-2 rounded-xl bg-blue-400 text-white hover:bg-red-600 transition-colors"></button>
+        </div>
+      <button
+        onClick={handleAudioReset}
+        className="specialBtn px-4 py-2 rounded-xl bg-red-500 text-white hover:bg-red-600 transition-colors">
+        Reset
+      </button>
+      <p className="italic text-slate-400">Ready to transcribe and translate</p>
     </main>
   );
 }
