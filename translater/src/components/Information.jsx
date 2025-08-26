338c2f875a09400fa4d60fea2fb33e09aececefa
madvin <madvin@abv.bg>
2025-08-26 11:47:52 +0300

    make information

--- translater/src/components/Information.jsx
+++ translater/src/components/Information.jsx
@@ -1,3 +1,9 @@
 export default function Information() {
-    return
+    return (
+        <main className="flex flex-1 p-4 flex-col items-center justify-center gap-3 text-center sm:gap-4 md:gap-5 pb-20 min-h-screen">
+            <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Your <span className="text-blue-400 bold font-semibold">File</span>
+            </h1>
+        </main>
+    )
 }
