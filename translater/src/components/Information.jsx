2cb4dcd2af21e2bea1e036e29a2154c81c728c44
madvin <madvin@abv.bg>
2025-08-26 11:49:04 +0300

    information transcription

--- translater/src/components/Information.jsx
+++ translater/src/components/Information.jsx
@@ -2,7 +2,7 @@ export default function Information() {
     return (
         <main className="flex flex-1 p-4 flex-col items-center justify-center gap-3 text-center sm:gap-4 md:gap-5 pb-20 min-h-screen">
             <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
-                Your <span className="text-blue-400 bold font-semibold">File</span>
+                Your <span className="text-blue-400 bold font-semibold">Transcription</span>
             </h1>
         </main>
     )
