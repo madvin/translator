a2441aa861c6191828c46266547bcdfb61e0ea81
madvin <madvin@abv.bg>
2025-08-28 20:27:49 +0300

    fix the toggle button styles and state management

--- translater/src/components/Information.jsx
+++ translater/src/components/Information.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 export default function Information() {
+    const [tab, setTab] = useState('transcription');
     return (
         <main className="flex flex-1 p-4 flex-col items-center justify-center gap-3 text-center sm:gap-4 md:gap-5 pb-20 max-w-prose w-full mx-auto">
             <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
                 Your <span className="text-blue-400 bold font-semibold">Transcription</span>
             </h1>
-            <div className="flex mx-auto bg-white border-2 border-solid border-blue-400 shadow rounded-full overflow-hidden items-center gap-4">
-                <button className="px-2 py-1 font-medium gap-2">Transcription</button>
-                <button className="px-2 py-1 font-medium gap-2">Translation</button>
+            <div className="flex mx-auto shadow rounded-full overflow-hidden items-center gap-2">
+                <button className={"px-4 py-1 font-medium " + 
+                    (tab === 'transcription' ? 
+                    "bg-blue-400 text-white" : 
+                    "text-blue-400 bg-white hover:text-blue-600")}>Transcription</button>
+                <button className={"px-4 py-1 font-medium " + 
+                    (tab === 'translation' ? 
+                    "bg-blue-400 text-white" : 
+                    "text-blue-400 bg-white hover:text-blue-600")}>Translation</button>
             </div>
-            
         </main>
     )
 }
