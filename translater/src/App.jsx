e3a5c76e929a009c9c2a31c07ebfaf31352a8f2a
madvin <madvin@abv.bg>
2025-09-01 20:35:26 +0300

    add whisper worker file

--- translater/src/App.jsx
+++ translater/src/App.jsx
@@ -8,6 +8,7 @@ import Transcribing from "./components/Transcribing";
 function App() {
     const [file, setFile] = useState(null);
     const [audioStream, setAudioStream] = useState(null);
+    const [downloading, setDownloading] = useState(false);
     const [output, setOutput] = useState(false);
     const [loading, setLoading] = useState(true);
 
@@ -18,9 +19,54 @@ function App() {
         setAudioStream(null);
     }
 
+    const worker = useRef(null);
+
     useEffect(() => {
-        console.log(audioStream);
-    }, [audioStream]);
+        if (!worker.current) {
+            worker.current = new Worker(new URL('./utils/worker.js', import.meta.url),
+                { type: 'module' });
+        } 
+        const onMessageReceived = async (e) => {
+            switch (e.data.type) {
+                case 'DOWNLOADING':
+                    setDownloading(true);
+                    console.log('Downloading model...');                    
+                    break;
+                    
+            }
+              switch (e.data.type) {
+                case 'LOADING':
+                    setLoading(true);
+                    console.log('Loading model...');
+                    break;
+            }
+              switch (e.data.type) {
+                case 'RESULT':
+                    setOutput(true);
+                    console.log('Setting output...');
+                    break;
+            }
+              switch (e.data.type) {
+                case 'INFERENCE_DONE':
+                    setFinished(true);
+                    console.log('Inference done...');
+                    break;
+            }
+        }
+        worker.current.addEventListener('message', onMessageReceived);
+
+        return () => worker.current.removeEventListener('message', onMessageReceived);
+
+    }, []); 
+
+    async function readAudioFrom(file) {
+        const sampling_rate = 16000;
+        const audioCTX = new AudioContext({ sampleRate: sampling_rate });
+        const response = await file.arrayBuffer();
+        const decoded = await audioCTX.decodeAudioData(response);
+        const audio = decoded.getChannelData(0);
+        return audio;
+    }
 
     return (
         <div className="flex  flex-col p-4 max-w-[1000px] mx-auto w-full">
