e3119ef482acdf6fd23320f4af561e9c89da5422
madvin <madvin@abv.bg>
2025-09-02 20:47:58 +0300

    finished whisper.worker.js

--- translater/src/utils/whisper.worker.js
+++ translater/src/utils/whisper.worker.js
@@ -4,7 +4,7 @@ class MyTracriptionPipeline {
     static task = 'automatic-speech-recognition';
     static model = 'openai/whisper-small';
     static instance = null;
-    
+
     static async getInstance() {
         if (this.instance === null) {
             this.instance = await pipeline(this.task, this.model);
@@ -14,10 +14,10 @@ class MyTracriptionPipeline {
 }
 
 self.addEventListener('message', async (event) => {
-    const {type, audio } = event.data;
+    const { type, audio } = event.data;
     if (type === 'MessageTypes.INFERENCE_REQUEST') {
         await transcribe(audio)
-    } 
+    }
 })
 async function transcribe(audio) {
     sendLoadingMessage('loading');
@@ -28,7 +28,7 @@ async function transcribe(audio) {
     } catch (error) {
         sendErrorMessage('Failed to load model: ' + error.message);
         console.log('Failed to load model: ', error);
-        
+
         return;
     }
     sendLoadingMessage('succsess');
@@ -50,10 +50,10 @@ async function transcribe(audio) {
 }
 
 async function load_model_callback(data) {
-    const {status} = data;
+    const { status } = data;
     if (status === 'progress') {
         sendLoadingMessage(file, progress, loaded, total);
-    }  
+    }
 }
 
 function sendLoadingMessage(status) {
@@ -61,4 +61,98 @@ function sendLoadingMessage(status) {
         type: 'MessageTypes.LOADING',
         status
     });
+}
+
+async function sendDownloadingMessage(file, progress, loaded, total) {
+    self.postMessage({
+        type: 'MessageTypes.DOWNLOADING',
+        file,
+        progress,
+        loaded,
+        total
+    });
+}
+
+class GeneratuionTracker {
+    constructor(pipeline, stride_length_s) {
+        this.pipeline = pipeline;
+        this.stride_length_s = stride_length_s;
+        this.chunks = [];
+        this.time_precision = pipeline?.processor.feature_extractor.config.chunk_length
+            / pipeline.model.config.max_seource_positions;
+        this.processed_chunks = [];
+        this.callbackFunctionnCounter = 0;
+    }
+    sendFinalResult() {
+        self.postMessage({ type: MessageTypes.INFERENCE_DONE })
+    }
+
+    callbackFunction(beams) {
+        this.callbackFunctionnCounter += 1;
+        if (this.callbackFunctionnCounter % 10 !== 0) {
+            return;
+        }
+        const bestBeam = beams[0];
+        let text = this.pipeline.tokenizer.decode(bestBeam,
+            autput_token_ids, {
+            skip_special_tokens: true,
+        })
+
+        const result = {
+            text,
+            start: this.getLastChunkTimestamp(),
+            end: undefined
+        }
+        createPartialResultMEssage(result);
+    }
+    chunkCallback(data) {
+        this.chunks.push(data);
+        const [text, { chunks }] = this.pipeline.tokenizer.decode_asr(
+            this.chunks,
+            {
+                time_precision: this.time_precision,
+                return_timestamps: true,
+                force_full_sequence: false
+            })
+
+        this.processed_chunks = chunks.map((chunk, index) => {
+            this.processed_chunks(chunk, index)
+        })
+
+        createResultMessage(
+            this.processed_chunks, false, this.getLastChunkTimestamp()
+        )
+    }
+
+    getLastChunkTimestamp() {
+        if (this.processed_chunks.length === 0) {
+            return 0;
+        }
+    }
+    proccessChunk(chunk, index) {
+        const { text, timestamp } = chunk;
+        const [start, end] = timestamp;
+        return {
+            index,
+            text: `${text.trim()}`,
+            start: Math.round(start),
+            end: Math.round(end) || Math.round(start + 0.9 * this.stride_length_s)
+        }
+    }
+}
+function createResultMessage(results, isDone, completeUntilTimestamp) {
+    self.postMessage({
+        type: 'MessageTypes.RESULT',
+        results,
+        isDone,
+        completeUntilTimestamp
+    })
+    function createPartialResultMEssage(result) {
+        self.postMessage({
+            type: 'MessageTypes.RESULT_PARTIAL',
+            result
+        })
+
+    }
+
 }
