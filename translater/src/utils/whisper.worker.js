1295c2ec85c52ac3fe2c545976d2093725d78da6
madvin <madvin@abv.bg>
2025-09-02 20:16:04 +0300

    add functions to handle audio buffer and blob

--- translater/src/utils/whisper.worker.js
+++ translater/src/utils/whisper.worker.js
@@ -12,3 +12,53 @@ class MyTracriptionPipeline {
         return this.instance;
     }
 }
+
+self.addEventListener('message', async (event) => {
+    const {type, audio } = event.data;
+    if (type === 'MessageTypes.INFERENCE_REQUEST') {
+        await transcribe(audio)
+    } 
+})
+async function transcribe(audio) {
+    sendLoadingMessage('loading');
+
+    let pipeline;
+    try {
+        pipeline = await MyTracriptionPipeline.getInstance();
+    } catch (error) {
+        sendErrorMessage('Failed to load model: ' + error.message);
+        console.log('Failed to load model: ', error);
+        
+        return;
+    }
+    sendLoadingMessage('succsess');
+
+    const stride_length_s = 5;
+
+    const generationTracker = new GeneratuionTracker(pipeline, stride_length_s);
+
+    await pipeline(audio, {
+        top_k: 0,
+        do_sample: false,
+        chunk_length_s: 30,
+        stride_length_s,
+        return_timestamps: true,
+        callback_function: generationTracker.callbackFunction.bind(generationTracker),
+        chunk_callback: generationTracker.chunkCallback.bind(generationTracker),
+    })
+    generationTracker.sendFinalResult();
+}
+
+async function load_model_callback(data) {
+    const {status} = data;
+    if (status === 'progress') {
+        sendLoadingMessage(file, progress, loaded, total);
+    }  
+}
+
+function sendLoadingMessage(status) {
+    self.postMessage({
+        type: 'MessageTypes.LOADING',
+        status
+    });
+}
