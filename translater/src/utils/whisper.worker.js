45f7b5b12d6e05869447a0d14f1079abde6cafac
madvin <madvin@abv.bg>
2025-09-04 20:57:19 +0300

    fixed more bugs in whisper worker

--- translater/src/utils/whisper.worker.js
+++ translater/src/utils/whisper.worker.js
@@ -7,9 +7,9 @@ class MyTranscriptionPipeline {
     static model = 'openai/whisper-small';
     static instance = null;
 
-    static async getInstance() {
+    static async getInstance(callback) {
         if (this.instance === null) {
-            this.instance = await pipeline(this.task, this.model);
+            this.instance = await pipeline(this.task, this.model, { progress_callback: callback });
         }
         return this.instance;
     }
@@ -17,29 +17,29 @@ class MyTranscriptionPipeline {
 
 self.addEventListener('message', async (event) => {
     const { type, audio } = event.data;
-    if (type === 'MessageTypes.INFERENCE_REQUEST') {
-        await transcribe(audio)
+    if (type === MessageTypes.INFERENCE_REQUEST) {
+        await transcribe(audio);
     }
-})
+});
+
 async function transcribe(audio) {
     sendLoadingMessage('loading');
 
-    let pipeline;
+    let modelPipeline;
     try {
-        pipeline = await MyTranscriptionPipeline.getInstance(load_model_callback);
+        modelPipeline = await MyTranscriptionPipeline.getInstance(load_model_callback);
     } catch (error) {
         sendErrorMessage('Failed to load model: ' + error.message);
-        console.log('Failed to load model: ', error);
-
+        console.error('Failed to load model: ', error);
         return;
     }
+
     sendLoadingMessage('success');
 
     const stride_length_s = 5;
+    const generationTracker = new GenerationTracker(modelPipeline, stride_length_s);
 
-    const generationTracker = new GenerationTracker(pipeline, stride_length_s);
-
-    await pipeline(audio, {
+    await modelPipeline(audio, {
         top_k: 0,
         do_sample: false,
         chunk_length_s: 30,
@@ -47,7 +47,8 @@ async function transcribe(audio) {
         return_timestamps: true,
         callback_function: generationTracker.callbackFunction.bind(generationTracker),
         chunk_callback: generationTracker.chunkCallback.bind(generationTracker),
-    })
+    });
+
     generationTracker.sendFinalResult();
 }
 
@@ -55,24 +56,31 @@ async function load_model_callback(data) {
     const { status } = data;
     if (status === 'progress') {
         const { file, progress, loaded, total } = data;
-        sendLoadingMessage(file, progress, loaded, total);
+        sendDownloadingMessage(file, progress, loaded, total);
     }
 }
 
 function sendLoadingMessage(status) {
     self.postMessage({
-        type: 'MessageTypes.LOADING',
-        status
+        type: MessageTypes.LOADING,
+        status,
     });
 }
 
-async function sendDownloadingMessage(file, progress, loaded, total) {
+function sendErrorMessage(message) {
     self.postMessage({
-        type: 'MessageTypes.DOWNLOADING',
+        type: MessageTypes.ERROR,
+        message,
+    });
+}
+
+function sendDownloadingMessage(file, progress, loaded, total) {
+    self.postMessage({
+        type: MessageTypes.DOWNLOADING,
         file,
         progress,
         loaded,
-        total
+        total,
     });
 }
 
@@ -81,13 +89,15 @@ class GenerationTracker {
         this.pipeline = pipeline;
         this.stride_length_s = stride_length_s;
         this.chunks = [];
-        this.time_precision = pipeline?.processor.feature_extractor.config.chunk_length
-            / pipeline.model.config.max_source_positions;
+        this.time_precision =
+            pipeline?.processor.feature_extractor.config.chunk_length /
+            pipeline.model.config.max_source_positions;
         this.processed_chunks = [];
         this.callbackFunctionCounter = 0;
     }
+
     sendFinalResult() {
-        self.postMessage({ type: MessageTypes.INFERENCE_DONE })
+        self.postMessage({ type: MessageTypes.INFERENCE_DONE });
     }
 
     callbackFunction(beams) {
@@ -95,65 +105,71 @@ class GenerationTracker {
         if (this.callbackFunctionCounter % 10 !== 0) {
             return;
         }
+
         const bestBeam = beams[0];
-        let text = this.pipeline.tokenizer.decode(bestBeam,
-            autput_token_ids, {
+        let text = this.pipeline.tokenizer.decode(bestBeam.output_token_ids, {
             skip_special_tokens: true,
-        })
+        });
 
         const result = {
             text,
             start: this.getLastChunkTimestamp(),
-            end: undefined
-        }
+            end: undefined,
+        };
+
         createPartialResultMessage(result);
     }
+
     chunkCallback(data) {
         this.chunks.push(data);
-        const [text, { chunks }] = this.pipeline.tokenizer._decode_asr(
-            this.chunks,
-            {
-                time_precision: this.time_precision,
-                return_timestamps: true,
-                force_full_sequence: false
-            })
+        const [text, { chunks }] = this.pipeline.tokenizer._decode_asr(this.chunks, {
+            time_precision: this.time_precision,
+            return_timestamps: true,
+            force_full_sequence: false,
+        });
 
-        this.processed_chunks = chunks.map((chunk, index) => {
-            return this.processed_chunks(chunk, index)
-        })
+        this.processed_chunks = chunks.map((chunk, index) =>
+            this.processChunk(chunk, index)
+        );
 
         createResultMessage(
-            this.processed_chunks, false, this.getLastChunkTimestamp()
-        )
+            this.processed_chunks,
+            false,
+            this.getLastChunkTimestamp()
+        );
     }
 
     getLastChunkTimestamp() {
         if (this.processed_chunks.length === 0) {
             return 0;
         }
+        return this.processed_chunks[this.processed_chunks.length - 1].end;
     }
-    proccessChunk(chunk, index) {
+
+    processChunk(chunk, index) {
         const { text, timestamp } = chunk;
         const [start, end] = timestamp;
         return {
             index,
-            text: `${text.trim()}`,
+            text: text.trim(),
             start: Math.round(start),
-            end: Math.round(end) || Math.round(start + 0.9 * this.stride_length_s)
-        }
+            end: Math.round(end) || Math.round(start + 0.9 * this.stride_length_s),
+        };
     }
 }
+
 function createResultMessage(results, isDone, completeUntilTimestamp) {
     self.postMessage({
-        type: 'MessageTypes.RESULT',
+        type: MessageTypes.RESULT,
         results,
         isDone,
-        completeUntilTimestamp
-    })
+        completeUntilTimestamp,
+    });
 }
+
 function createPartialResultMessage(result) {
-        self.postMessage({
-            type: 'MessageTypes.RESULT_PARTIAL',
-            result
-        })
+    self.postMessage({
+        type: MessageTypes.RESULT_PARTIAL,
+        result,
+    });
 }
